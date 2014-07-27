5befe32dbc650621fab7784dc7219524bc949c58
Tom <codertomo@gmail.com>
2014-07-27 08:58:33 -0700

    Completed requirements.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        separator: ';', 
+      }, 
+      dist: {
+          src: ['public/client/*.js'],
+          dest: 'public/dist/built.js'
+      }
     },
 
     mochaTest: {
@@ -21,12 +28,15 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      my_target: {
+        files: {
+          'public/dist/built.min.js': ['public/dist/built.js']
+        }
+      }
     },
 
     jshint: {
-      files: [
-        // Add filespec list here
-      ],
+      all: ['public/client/*.js'],
       options: {
         force: 'true',
         jshintrc: '.jshintrc',
@@ -93,7 +103,7 @@ module.exports = function(grunt) {
     'mochaTest'
   ]);
 
-  grunt.registerTask('build', [
+  grunt.registerTask('build', ['concat', 'uglify'
   ]);
 
   grunt.registerTask('upload', function(n) {
